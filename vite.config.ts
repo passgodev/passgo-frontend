61869816a049d7dd150b1f46cd3f27c743372241
Pajtus <piotrus.umanski@gmail.com>
2025-04-22 15:53:39 +0200

    feat(project): deleted generated basic code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const CONFIGURATION = {
+  protocol: 'http',
+  hostname: 'localhost',
+  port: 8080,
+  pathPrefix: 'rest-api' 
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `${CONFIGURATION.protocol}://${CONFIGURATION.hostname}:${CONFIGURATION.port}/${CONFIGURATION.pathPrefix}`,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
