ede9aa5dbda0ba4badb8589ab1e580982aae8c9a
Pajtus <piotrus.umanski@gmail.com>
2025-04-24 13:43:31 +0200

    feat(authRoutes): surrounded App component with AuthProvider, created basic routes for home page and login page, protected home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import Layout from './components/Layout.tsx';
+import RequireAuth from './components/RequireAuth.tsx';
+import Home from './pages/Home.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+import { Routes, Route } from 'react-router-dom';
+
 const App = () => {
     return (
-        <div>
-            <span>Hello World</span>
-        </div>
+        <Routes>
+            <Route path="/" element={<Layout />} >
+
+                {/*public routes*/}
+                <Route path="/login" element={<LoginPage />} />
+
+                {/*protected routes*/}
+                <Route element={<RequireAuth />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
+
+            </Route>
+        </Routes>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { AuthProvider } from './context/AuthProvider.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+    </StrictMode>
 )
