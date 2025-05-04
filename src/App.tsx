0df5a86970721aa1a48cb332e3c9154f2bf736c6
Pajtus <piotrus.umanski@gmail.com>
2025-05-04 22:13:11 +0200

    feat(appRouting): created test route for logged in member with specified roles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import LoginPage from './page/login/LoginPage.tsx';
 import ActiveMemberProfilePage from './page/member/ActiveMemberProfilePage.tsx';
 import ClientInfoPage from './page/member/client/ClientInfoPage.tsx';
 import SignupPage from './page/signup/SignupPage.tsx';
+import TestOnlyAuthorized from './page/TestOnlyAuthorized.tsx';
 import Transaction from './page/transaction/Transaction.tsx';
+import UnauthorizedPage from './page/UnauthorizedPage.tsx';
 
 
 const App = () => {
@@ -15,18 +17,25 @@ const App = () => {
         <Routes>
             <Route path="/" >
 
-                {/*public routes*/}
+                {/* public routes */}
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
+                <Route path="/unauthorized" element={<UnauthorizedPage />} />
 
-                {/*protected routes*/}
+                {/* protected routes */}
                 <Route element={<PersistLogin />} >
+                    {/* member with no set role(memberType) can access */}
                     <Route element={<RequireAuth />}>
                         <Route element={<Layout />} >
                             <Route path="/" element={<Home />} />
                             <Route path="/transaction" element={<Transaction />} />
                             <Route path="/clients/:id" element={<ClientInfoPage />} />
                             <Route path="/members/me" element={<ActiveMemberProfilePage />} />
+                        </Route>
+                    </Route>
+                    <Route element={<RequireAuth allowedRoles={['ADMINISTRATOR']} />} >
+                        <Route element={<Layout />} >
+                            <Route path='testauthorize' element={<TestOnlyAuthorized/>} />
                         </Route>
                     </Route>
                 </Route>
