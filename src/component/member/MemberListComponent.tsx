65c963f07e9014a1a0ff8fc251417c3aa1490c61
Pajtus <piotrus.umanski@gmail.com>
2025-05-11 13:41:54 +0200

    refactor(memberListComponent): refactored memberListComponent styling, refactored common code for fetching members

--- src/component/member/MemberListComponent.tsx
+++ src/component/member/MemberListComponent.tsx
@@ -1,6 +1,7 @@
-import { Typography } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import { Box, Button, Paper, Typography } from '@mui/material';
 import { DataGrid, GridColDef, GridPaginationInitialState, GridValidRowModel } from '@mui/x-data-grid';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useInterceptedFetch from '../../hook/useInterceptedFetch.ts';
 import MemberType from '../../model/member/MemberType.ts';
 import API_ENDPOINTS from '../../util/endpoint/ApiEndpoint.ts';
@@ -16,7 +17,7 @@ const MemberListComponent = <T extends GridValidRowModel,> ({memberType, columns
     const interceptedFetch = useInterceptedFetch();
     const [paginatedMembers, setPaginatedMembers] = useState<Paginated<T>>();
 
-    useEffect(() => {
+    const loadMember = useCallback(() => {
         interceptedFetch({
             endpoint: API_ENDPOINTS.members.replace(':memberType', MemberType[memberType])
         }).then(res => res.json() as unknown as Paginated<T> )
@@ -24,6 +25,10 @@ const MemberListComponent = <T extends GridValidRowModel,> ({memberType, columns
                 console.log('Paginated members', paginatedMembers);
                 setPaginatedMembers(paginatedMembers);
             })
+    }, []);
+
+    useEffect(() => {
+        loadMember();
     }, []);
 
     const members = paginatedMembers?.content;
@@ -37,22 +42,39 @@ const MemberListComponent = <T extends GridValidRowModel,> ({memberType, columns
 
     return (
         <>
-            {
-                members === undefined
-                    ? <Typography>Loading...</Typography>
-                    : members.length === 0
-                        ? <Typography>No members found.</Typography>
-                        : <DataGrid
-                            rows={members}
-                            columns={columns}
-                            initialState={{
-                                pagination
-                            }}
-                            pageSizeOptions={[1, 5, 10]}
-                            checkboxSelection
-                            disableRowSelectionOnClick
-                        />
-            }
+            <Paper elevation={2}>
+                <Box flexDirection='column' padding='20px'>
+                    <Box display='flex' flexDirection='row' justifyContent='space-between' marginBottom={2} sx={{ bgcolor: 'primary.main' }} padding={1} borderRadius={1}>
+                        <Box width='100%' display='flex' justifyContent='center' >
+                            <Typography variant="h4" fontWeight="bold" color='white'>
+                                {MemberType[memberType]}
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                            <Button title='Refresh' size='small' onClick={() => loadMember()} >
+                                <RefreshIcon sx={{color: 'white'}} fontSize='small' />
+                            </Button>
+                        </Box>
+                    </Box>
+                    {
+                        members === undefined
+                            ? <Typography>Loading...</Typography>
+                            : members.length === 0
+                                ? <Typography>No members found.</Typography>
+                                : <DataGrid
+                                    rows={members}
+                                    columns={columns}
+                                    initialState={{
+                                        pagination
+                                    }}
+                                    pageSizeOptions={[1, 5, 10]}
+                                    checkboxSelection
+                                    disableRowSelectionOnClick
+                                />
+                    }
+                </Box>
+
+            </Paper>
         </>
     );
 };
