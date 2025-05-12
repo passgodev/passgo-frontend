8d1bb73eb4f86c319642302b4eb8cf04529d18d7
Pajtus <piotrus.umanski@gmail.com>
2025-05-12 12:35:50 +0200

    refactor(clientInfoPage): changed page about client and its information

--- src/page/member/client/ClientInfoPage.tsx
+++ src/page/member/client/ClientInfoPage.tsx
@@ -1,5 +1,21 @@
 import { useParams } from 'react-router-dom';
+import MemberInformationComponent from '../../../component/member/MemberInformationComponent.tsx';
+import LatestTransactionsComponent from '../../../component/transaction/LatestTransactionsComponent.tsx';
+import MemberCardWrapper from '../MemberCardWrapper.tsx';
 
+const user = {
+    firstName: 'Jan',
+    lastName: 'Kowalski',
+    role: 'administrator',
+    age: 30,
+    address: 'ul. Kwiatowa 15, 00-123 Warszawa',
+    avatar: 'https://i.pravatar.cc/150?img=3',
+    transactions: [
+        { id: 'TXN123', amount: '150.00 PLN', date: '2024-05-10' },
+        { id: 'TXN122', amount: '89.99 PLN', date: '2024-05-07' },
+        { id: 'TXN121', amount: '240.50 PLN', date: '2024-05-01' },
+    ],
+};
 
 const ClientInfoPage = () => {
     const params = useParams();
@@ -7,7 +23,13 @@ const ClientInfoPage = () => {
     console.log('client info page params', params);
 
     return (
-        <div>ClientInfoPage</div>
+        <MemberCardWrapper>
+            {/* Sekcja: O użytkowniku */}
+            <MemberInformationComponent member={{...user}} />
+
+            {/* Sekcja: Ostatnie transakcje */}
+            <LatestTransactionsComponent transactions={user.transactions} />
+        </MemberCardWrapper>
     );
 };
 export default ClientInfoPage;
