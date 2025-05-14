cdea00efd84f64a8ce324beb8d8588ff5eca99f5
Pajtus <piotrus.umanski@gmail.com>
2025-05-14 21:14:23 +0200

    refactor(unauthorizedPage): utilized fallbackCard component

--- src/page/UnauthorizedPage.tsx
+++ src/page/UnauthorizedPage.tsx
@@ -1,36 +1,15 @@
-import { Box, Card, CssBaseline, Divider, Typography } from '@mui/material';
-import Link from '@mui/material/Link';
 import { useNavigate } from 'react-router-dom';
+import FallbackCard from '../component/page/FallbackCard.tsx';
 
 
 const UnauthorizedPage = () => {
     const navigate = useNavigate();
 
     return (
-        <Box display={'flex'} justifyContent={'center'} alignItems={'center'} sx={{bgcolor: 'primary.main', width: '100vw', height: '100vh', m: 0, p: 0}}>
-            <CssBaseline />
-            <Card sx={{ p: 8 }}>
-                <Typography color='primary' variant='h2'>
-                    Passgo
-                </Typography>
-                <Divider sx={{ mb: 2 }} />
-                <Typography align='center' variant='h5' noWrap>
-                    Unauthorized page
-                </Typography>
-                <Box display='flex' justifyContent={'center'}>
-                    <Typography noWrap>
-                        Return To {
-                        <Typography component={Link} onClick={(e) => {
-                            e.preventDefault();
-                            navigate(-1)
-                        }}>
-                            previous page
-                        </Typography>
-                    }
-                    </Typography>
-                </Box>
-            </Card>
-        </Box>
+        <FallbackCard title='Unauthorized page' navigateTo={{
+            name: 'Unauthorized Page',
+            callback: () => navigate(-1)
+        }} />
     );
 };
 export default UnauthorizedPage;
