8c9d0715521d582e0e52885d68b768774ca5eb6a
Dorajn <derquv@gmail.com>
2025-05-18 18:50:56 +0200

    resolving massive conflicts

--- src/page/member/client/ClientInfoPage.tsx
+++ src/page/member/client/ClientInfoPage.tsx
@@ -1,13 +1,40 @@
+import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import AlertContext from '../../../context/AlertProvider.tsx';
+import useInterceptedFetch from '../../../hook/useInterceptedFetch.ts';
+import ClientDto from '../../../model/client/ClientDto.ts';
+import MemberType from '../../../model/member/MemberType.ts';
+import API_ENDPOINTS from '../../../util/endpoint/ApiEndpoint.ts';
+import ClientInfoComponent from './ClientInfoComponent.tsx';
 
 
 const ClientInfoPage = () => {
     const params = useParams();
+    console.log('ClientInfoPage', params)
+    const interceptedFetch = useInterceptedFetch();
+    const { showAlert } = useContext(AlertContext);
+    const [client, setClient] = useState<ClientDto>();
 
-    console.log('client info page params', params);
+    useEffect(() => {
+        const endpoint = API_ENDPOINTS.memberById
+            .replace(':id', params.id ?? '-1')
+            .replace(':memberType', MemberType[MemberType.CLIENT]);
+        console.log('ClientInfoPage - useEffect invoked', 'endpoint', endpoint);
+
+        interceptedFetch({endpoint})
+            .then(res => res.json())
+            .then(json => {
+                console.log('ClientInfoPage - returned json', json);
+                setClient(json)
+            })
+            .catch(err => {
+                console.log('ClientInfoPage - err', err)
+                showAlert(err, 'error');
+            });
+    }, []);
 
     return (
-        <div>ClientInfoPage</div>
+        <ClientInfoComponent client={client!} />
     );
 };
 export default ClientInfoPage;
