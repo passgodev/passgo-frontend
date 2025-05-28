b10ca2fc79f832b7576fd64d9b65eb35bfd74cf3
Pajtus <piotrus.umanski@gmail.com>
2025-05-28 22:07:37 +0200

    refactor(useRefreshedTokenHook): added better logging

--- src/hook/useRefreshToken.ts
+++ src/hook/useRefreshToken.ts
@@ -2,6 +2,7 @@ import { Auth } from '../context/AuthProvider.tsx';
 import { retrieveMemberId, transferMemberTypeToPrivilege } from '../util/AccessTokenUtil.ts';
 import ApiEndpoint from '../util/endpoint/ApiEndpoint.ts';
 import HttpMethod from '../util/HttpMethod.ts';
+import { loggerPrelogWithFactory } from '../util/logger/Logger.ts';
 import useAuth from './useAuth';
 
 
@@ -10,11 +11,16 @@ interface RefreshResponse {
     token: string;
 }
 
+const logger = loggerPrelogWithFactory('[useRefreshToken]')
+
+
 const UseRefreshToken = () => {
     const { auth, setAuth } = useAuth();
     const refreshToken = auth.refreshToken ?? localStorage.getItem('passgoRT');
 
     const refresh = async () => {
+        logger.log('invoked');
+
         const headers = new Headers();
         headers.append("Content-Type", "application/json");
 
@@ -30,25 +36,32 @@ const UseRefreshToken = () => {
             const text = await res.text();
 
             if ( res.status === 200 ) {
+                logger.log('Backend responded with code 200', 'response', res);
                 return JSON.parse(text) as RefreshResponse;
+            } else {
+                logger.log('Backend responded with code != 200', 'response', res);
+                throw 'useRefreshTokenError';
             }
-            throw 'useRefreshTokenError';
         });
 
-        setAuth(prev => {
-            console.log('prev auth - useRefreshToken', JSON.stringify(prev));
-            console.log('response auth - useRefreshToken', JSON.stringify(response));
+        logger.log('Received response', response);
 
+        setAuth(prev => {
             const token = response.token;
             const privilege = transferMemberTypeToPrivilege(token);
             const memberId = retrieveMemberId(token);
 
-            return {
+            const newAuth = {
                 ...prev,
                 privilege,
                 memberId,
                 token
             } as Auth;
+
+            logger.log('Prev auth - useRefreshToken', prev);
+            logger.log('Current auth - useRefreshToken', newAuth);
+
+            return newAuth
         });
 
         return response.token;
